593cd19bcbe415abe90fadd9a0a8fbc29923bc48
SalemElatar <salem.elatar5@gmail.com>
2022-09-10 06:51:38 +0200

    #1: Creating Project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+// import HelloPage from "./components/HelloPage.vue";
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
